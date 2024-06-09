f28588ee04cfa264339ddb1c18af27a72f8ed300
Muaibed <j.muaibd@gmail.com>
2024-06-09 23:20:22 +0300

    adding the medical history card

--- healthcare_provider/src/app/dashboard/page.tsx
+++ healthcare_provider/src/app/dashboard/page.tsx
@@ -43,7 +43,7 @@ export default function Dashboard() {
                         <TimeLine />
                     </div>
                 </div>
-            <div className="flex">
+            <div>
                 <div className="flex pt-3">
                     <div className="w-96 h-[21rem] bg-white rounded mr-2">
                     <div className="flex p-2">
@@ -55,6 +55,7 @@ export default function Dashboard() {
                         <Calendar className="flex justify-center w-full"></Calendar>
                     </div>
                 </div>
+        <MedicalHistory />
             </div>
         </div>
     </div>
@@ -260,5 +261,48 @@ function TimeLine() {
           </div>
         </div>
       </div>
+    )
+}
+
+function MedicalHistory() {
+    return (
+        <div className="flex pt-3">
+            <div className="w-96 h-[20.5rem] bg-white rounded mr-2">
+                <div className="flex p-2">
+                    <div className="pl-5 pt-5">
+                        <RoundedImage imagePath="/logo1.png" width={40} height={40}/>
+                    </div>
+                    <div className="pl-5 pt-6 text-lg font-bold italic">Medical History
+                    </div>
+                </div>
+                <div className="flex p-2">
+                    <div className="pl-5 pt-3">
+                        <RoundedImage imagePath="/logo1.png" width={30} height={30}/>
+                    </div>
+                    <div className="pt-3 pl-3">
+                        <div className="font-bold text-sm">Chronic Disease</div>
+                        <div className="text-gray-300 text-[11px]">Diabetes, Hypertension, Asthma, Neurological Disorders</div>
+                    </div>
+                </div>
+                <div className="flex p-2 bg-blue-600/[.06]">
+                    <div className="pl-5 pt-3">
+                        <RoundedImage imagePath="/logo1.png" width={30} height={30}/>
+                    </div>
+                    <div className="pt-3 pl-3">
+                        <div className="font-bold text-sm">Chronic Disease</div>
+                        <div className="text-gray-300 text-[11px]">Diabetes, Hypertension, Asthma</div>
+                    </div>
+                </div>
+                <div className="flex p-2">
+                    <div className="pl-5 pt-3">
+                        <RoundedImage imagePath="/logo1.png" width={30} height={30}/>
+                    </div>
+                    <div className="pt-3 pl-3">
+                        <div className="font-bold text-sm">Chronic Disease</div>
+                        <div className="text-gray-300 text-[11px]">Diabetes, Hypertension, Asthma</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
