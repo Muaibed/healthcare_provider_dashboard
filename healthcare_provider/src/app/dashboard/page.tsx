fca35af5c6ffe8b5b970821cfcc8e3f393e0e714
Muaibed <j.muaibd@gmail.com>
2024-06-10 04:38:03 +0300

    redesigning the timeline component

--- healthcare_provider/src/app/dashboard/page.tsx
+++ healthcare_provider/src/app/dashboard/page.tsx
@@ -207,60 +207,86 @@ function InfoCard() {
 }
 
 function TimeLine() {
-    return ( 
-      <div className="p-6 sm:p-10">
-        <div className="after:absolute after:inset-y-0 after:w-px after:bg-gray-500/20 relative pl-6 after:left-0 grid gap-10 dark:after:bg-gray-400/20">
-          <div className="grid gap-1 text-sm relative">
-            <div className="aspect-square w-3 bg-gray-900 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1 dark:bg-gray-50" />
-            <div className="font-medium">March 14, 2024 - Pre diabetic</div>
-            <div className="text-gray-500 dark:text-gray-400">
-              Alc:10.4
-            </div>
-          </div>
-          <div className="grid gap-1 text-sm relative">
-            <div className="aspect-square w-3 bg-gray-900 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1 dark:bg-gray-50" />
-            <div className="font-medium">March 14, 2024 - Pre diabetic</div>
-            <div className="text-gray-500 dark:text-gray-400">
-              Alc:10.4
-            </div>
-          </div>
-          <div className="grid gap-1 text-sm relative">
-            <div className="aspect-square w-3 bg-gray-900 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1 dark:bg-gray-50" />
-            <div className="font-medium">March 14, 2024 - Pre diabetic</div>
-            <div className="text-gray-500 dark:text-gray-400">
-              Alc:10.4
-            </div>
-          </div>
-          <div className="grid gap-1 text-sm relative">
-            <div className="aspect-square w-3 bg-gray-900 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1 dark:bg-gray-50" />
-            <div className="font-medium">March 14, 2024 - Pre diabetic</div>
-            <div className="text-gray-500 dark:text-gray-400">
-              Alc:10.4
-            </div>
-          </div>
-          <div className="grid gap-1 text-sm relative">
-            <div className="aspect-square w-3 bg-gray-900 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1 dark:bg-gray-50" />
-            <div className="font-medium">March 14, 2024 - Pre diabetic</div>
-            <div className="text-gray-500 dark:text-gray-400">
-              Alc:10.4
-            </div>
-          </div>
-          <div className="grid gap-1 text-sm relative">
-            <div className="aspect-square w-3 bg-gray-900 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1 dark:bg-gray-50" />
-            <div className="font-medium">March 14, 2024 - Pre diabetic</div>
-            <div className="text-gray-500 dark:text-gray-400">
-              Alc:10.4
-            </div>
-          </div>
-          <div className="grid gap-1 text-sm relative">
-            <div className="aspect-square w-3 bg-gray-900 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1 dark:bg-gray-50" />
-            <div className="font-medium">March 14, 2024 - Pre diabetic</div>
-            <div className="text-gray-500 dark:text-gray-400">
-              Alc:10.4
-            </div>
-          </div>
+    return (
+        <div className="-my-6 pt-7">
+            <div className="relative pl-8 sm:pl-32 py-6 group">
+                <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600">
+                <div className="text-md">
+                <div className="flex justify-center">12Wed</div>
+                <div className="flex justify-center">Jun, 2024</div>
+                </div>
+                </time>
+            <div className="text-sm font-bold text-slate-900">Pre diabetic</div>
         </div>
-      </div>
+        <div className="text-slate-500 text-[11px]">Alc:10.4</div>
+    </div>
+    
+    <div className="relative pl-8 sm:pl-32 py-6 group bg-blue-600/[.06]">
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600">
+            <div className="text-md">
+                <div className="flex justify-center">12Wed</div>
+                <div className="flex justify-center">Jun, 2024</div>
+                </div>
+            </time>
+            <div className="text-sm font-bold text-slate-900">Pre diabetic</div>
+        </div>
+        <div className="text-slate-500 text-[11px]">Alc:10.4</div>
+    </div>
+    
+    <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600">
+            <div className="text-md">
+                <div className="flex justify-center">12Wed</div>
+                <div className="flex justify-center">Jun, 2024</div>
+                </div>
+            </time>
+            <div className="text-sm font-bold text-slate-900">Pre diabetic</div>
+        </div>
+        <div className="text-slate-500 text-[11px]">Alc:10.4</div>
+    </div>
+
+    <div className="relative pl-8 sm:pl-32 py-6 group bg-blue-600/[.06]">
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600">
+            <div className="text-md">
+                <div className="flex justify-center">12Wed</div>
+                <div className="flex justify-center">2022</div>
+                </div>
+            </time>
+            <div className="text-sm font-bold text-slate-900">Pre diabetic</div>
+        </div>
+        <div className="text-slate-500 text-[11px]">Alc:10.4</div>
+    </div>
+
+    <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600">
+            <div className="text-md">
+                <div className="flex justify-center">12Wed</div>
+                <div className="flex justify-center">Jun, 2024</div>
+                </div>
+            </time>
+            <div className="text-sm font-bold text-slate-900">Pre diabetic</div>
+        </div>
+        <div className="text-slate-500 text-[11px]">Alc:10.4</div>
+    </div>
+
+    <div className="relative pl-8 sm:pl-32 py-6 group bg-blue-600/[.06]">
+        <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600">
+            <div className="text-md">
+                <div className="flex justify-center">12Wed</div>
+                <div className="flex justify-center">Jun, 2024</div>
+                </div>
+            </time>
+            <div className="text-sm font-bold text-slate-900">Pre diabetic</div>
+        </div>
+        <div className="text-slate-500 text-[11px]">Alc:10.4</div>
+    </div>
+</div>
     )
 }
 
